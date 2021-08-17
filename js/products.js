8cd84fa6b2277d904768bf4bd8b7c64ff386c418
nano2412 <85708846+nano2412@users.noreply.github.com>
2021-08-17 11:57:19 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,4 +1,5 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
+// USAR ESTE ARCHIVO PARA EL PRÓXIMO EJERCICIO
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
