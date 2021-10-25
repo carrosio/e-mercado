2f00eba930131fb45632abb200f1c08b545f1073
nano2412 <85708846+nano2412@users.noreply.github.com>
2021-10-24 22:24:11 -0300

    base

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,34 @@
 const main = document.getElementById("item-box-carrito");
+let SelectedCurrency = "USD"
 
-function showItems(selector, countVal, items) {
+function showItems(items) {
+
     for (let x = 0; x < items.length; x++) {
         let id = `qty${x}`;
         let newBox = document.createElement("div");
+        let unitCost = items[x].unitCost
+        let currency = items[x].currency
+        let nameItm = items[x].name
+        let qty = items[x].count
+        
+        // pasar a dolares
+
+        if (SelectedCurrency != currency){
+            if (SelectedCurrency == "USD") {
+                unitCost /= 40
+                currency = "USD"
+            }
+            else {
+
+                unitCost *= 40
+                currency = "UYU"
+            }
+        }
+
         newBox.setAttribute("name", "nameBox");
+        newBox.setAttribute("class", "nameBoxItems  list-group-item list-group-item-action")
         main.appendChild(newBox);
+        
 
        /*  let count = items[x].count;
         if (selector == x) {
@@ -13,32 +36,65 @@ function showItems(selector, countVal, items) {
         }
  */
         newBox.innerHTML =
-            `<h4>${items[x].name}</h2>` +
-            `<h6>${items[x].unitCost} ${items[x].currency}</h5>` +
-            `<h5>Cantidad: ` +
-            `<input min="1" max="10" id="${id}" value="${items[x].count}" name="qtyEntry" type="number"></input>` +
+            `<div class="containerBox"><h5>${nameItm}</h2>` +
+            `<h6>${unitCost} ${currency}</h5>` +
+            `<h5>` +
+
+            `<input class="QtyNumber" min="1" max="10" id="${id}" value="${qty}" name="qtyEntry" type="number"></input>` +
             `</h5>` +
-            `<h5 id="SubTot${x}">SubTotal: ${items[x].count * items[x].unitCost}</h5>`;
+            `<h6 class="style-text white-box" id="SubTot${x}">Subtotal: ${qty * unitCost} ${""} ${currency}</h5>
+            </div>`
+             +
+            `<div class=""><img class="item-image img-thumbnail" src="${items[x].src}">`;
     }
 }
 
 function subTotal(items, id, countVal) {
-    let subTotal = countVal * items[id].unitCost;
-    document.getElementById(`SubTot${id}`).innerHTML = `Subtotal: ${subTotal}`
+    let cost = items[id].unitCost
+    let unitCost = items[id].unitCost
+    let currency = items[id].currency
+    
+    if (SelectedCurrency != currency){
+        if (SelectedCurrency == "USD") {
+            cost = unitCost  / 40
+            
+        }
+        if (SelectedCurrency == "UYU") {
+            cost = unitCost * 40
+        }
+    }
+    
+    console.log(cost)
+    let subTotal = countVal * cost;
+    document.getElementById(`SubTot${id}`).innerHTML = `Subtotal: ${subTotal}${" "}${SelectedCurrency}`
 }
 
 document.addEventListener("DOMContentLoaded", async function (e) {
     localStorage.getItem("userFunction");
-    let carBuy = (await getJSONData(CART_BUY_URL2)).data;
-    let car_articles = carBuy.articles;
     
-    showItems(null, null, car_articles);
+    let cartBuy = (await getJSONData(CART_BUY_URL2)).data;
+    let articlesList = cartBuy.articles;
     
+    
+    // show items
+    showItems(articlesList);
+   
+    // cambio de moneda
+    let currencySelect = document.getElementById("select-currency")
+    currencySelect.onchange = function(){
+        main.innerHTML = ''
+        let selected = currencySelect.options.selectedIndex
+        SelectedCurrency = currencySelect.options[selected].value
+        showItems(articlesList)
+    }
+
     let selectors = document.getElementsByName("qtyEntry");
+    // asignar ID a cada producto && permitir cambios
     for (let x = 0; x < selectors.length; x++) {
         let selectorSelected = document.getElementById(`qty${x}`);
         selectorSelected.onchange = function () {
-            subTotal(car_articles, x, selectorSelected.value);
+            subTotal(articlesList, x, selectorSelected.value);
+            
         };
     }
 });
--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,64 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
+
+let infoUSER = {
+    "name": "pepe" ,
+    "lastname": null ,
+    "age": null ,
+    "mail": null,
+    "phone": null
+}
+
+localStorage.setItem("infoUser", infoUSER)
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    
+
+    let editIcon = document.getElementsByClassName("fa-edit")
+    let values = document.getElementsByClassName("name-profile")
+
+    
+    for (let x = 0; x < values.length; x++) {
+        
+        let inputSaved = null
+        values[x].innerHTML = inputSaved
+        if (!inputSaved) {
+            values[x].innerHTML = `<p>¡Campo sin completar!</p>`
+        } 
+    }
+
+    for (let x = 0; x < editIcon.length; x++) {
+        editIcon[x].onclick = function(){
+            
+            let type
+
+            if (x == 2 || x == 4) {
+                type = "number"
+            }
+            else if (x == 3){
+                type = "email"
+            }
+            else {
+                type = "text"
+            }
+            values[x].innerHTML = `<input id="input${x}"type="${type}">  </input><i id="apply-input" class="fas fa-check-circle fa-1.8x"></i>`
+            
+            document.getElementById("apply-input").onclick = function() {
+                const newValue = document.getElementById(`input${x}`).value
+                if (!newValue) {
+                    alert("Ingrese un dato valido")
+                }
+                else {
+                    values[x].innerHTML = newValue
+                }
+                
+            }
+        }
+    }
+    
+  
 
 });
